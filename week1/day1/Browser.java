221a5482d9eec8da2c84703e51081914d7ba2118
vijiraghavmurugan <119110225+vijiraghavmurugan@users.noreply.github.com>
2023-03-02 11:00:54 +0530

    Add files via upload

---
+++ week1/day1/Browser.java
@@ -0,0 +1,10 @@
+package week1.day1.assignment;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is my browser");
+	}
+
+}

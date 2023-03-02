67c2b5e3e43b70c06738979d66b21af55629c2dc
vijiraghavmurugan <119110225+vijiraghavmurugan@users.noreply.github.com>
2023-03-02 11:12:57 +0530

    Create CommonMethods.java

---
+++ testcase/CommonMethods.java
@@ -0,0 +1,35 @@
+package testcase;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+
+public class CommonMethods {
+
+	public ChromeDriver driver;
+
+	@Parameters({"url","username","password"})
+	@BeforeMethod
+	public void PreConditions(String url,String username,String password) {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.findElement(By.id("username")).sendKeys(username);
+		driver.findElement(By.id("password")).sendKeys(password);
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+	}
+
+	@AfterMethod
+	public void PostConditions() {
+		driver.close();
+	}
+
+}

3f171f060fd29df9c4e61ad457051714553e3cda
vijiraghavmurugan <119110225+vijiraghavmurugan@users.noreply.github.com>
2023-03-02 10:59:19 +0530

    Create Chrome.java

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float version =100.2f;
+String name="chrome";
+boolean visible=true;
+int year=1998;
+char logo='m';
+
+System.out.println("Details about Chrome : ");
+System.out.println("Name : "+name);
+System.out.println("Version : "+version);
+System.out.println("Visible : "+visible);
+System.out.println("Year : "+year);
+System.out.println("Logo : "+logo);
+	}
+
+}
